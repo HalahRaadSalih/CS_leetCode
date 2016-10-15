0fd7c4a330e2c0000c4ce6c78c1c29148c4a4d83
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-10-14 21:04:40 -0700

    add solution to leverTraversal

---
+++ trees/levelTraversal.js
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    var result = [];
+    var stack = [];
+    var queue = [];
+
+    var currentLevel = 1;
+    var nextLevel = 0;
+
+    if(!root){
+        return result;
+    }
+
+    queue.push(root);
+
+    while(queue.length !==0 ){
+        // dequeue node from q
+        var node = queue.shift();
+        // push its value to temp stack
+        stack.push(node.val);
+        //decrease current level
+        currentLevel--;
+        
+        // if node has left, enqueue it
+        if(node.left){
+            queue.push(node.left);
+            //inc next level
+            nextLevel++;
+        }
+        // if node has right, enqueue it
+        if(node.right){
+            queue.push(node.right);
+            //inc next level
+            nextLevel++;
+        }
+
+        //level is zero?
+        if(currentLevel === 0){
+            //push contents of stack to result
+            result.push(stack);
+            currentLevel = nextLevel;
+            // start over
+            nextLevel = 0;
+            stack = [];
+        }
+
+    }
+
+    return result;
+};
