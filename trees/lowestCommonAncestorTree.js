b25a93248f3c3d4164f0023d4d36aa168faf4be5
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-10-14 19:54:48 -0700

    add solution to lowest common ancestor trree

---
+++ trees/lowestCommonAncestorTree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(!p || !q){
+        return null;
+    }
+
+    if(!root || root === p || root === q){
+        return root;
+    }
+
+    var left = lowestCommonAncestor(root.left, p, q);
+    var right = lowestCommonAncestor(root.right, p, q);
+
+    if(left && right ) {
+        return root;
+    }
+
+    return left|| right;
+};
